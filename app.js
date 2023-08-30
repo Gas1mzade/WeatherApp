e9dccd74b810425c4dfff4b185872eaf87f945f7
Gas1mzade <qasimovnihat3@gmail.com>
2023-08-30 15:35:41 +0000

    last fixed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 "use strict";
 $(document).ready(function () {
 
-;
+    ;
     $(document).on('keypress', function (e) {
-        if(e.which == 13){
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&APPID=07f2ff58f276f6c76273c618311f64ab`
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&APPID=07f2ff58f276f6c76273c618311f64ab&units=metric`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
@@ -18,9 +18,9 @@ $(document).ready(function () {
         }
     });
 
-    $("#btn").click(function (e) { 
+    $("#btn").click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&APPID=07f2ff58f276f6c76273c618311f64ab`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&APPID=07f2ff58f276f6c76273c618311f64ab&units=metric`
         async function myweather() {
             let responce = await fetch(url)
             let data = await responce.json()
